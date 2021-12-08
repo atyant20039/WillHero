936eebc681ebfeaf2048603af8d5d7fd0dbc6c98
Atyant Sony <atyantsony2020@gmail.com>
2021-12-08 23:19:55 +0530

    The floating platforms in background are moving up and down now.

--- src/main/java/com/example/willhero/gameController.java
+++ src/main/java/com/example/willhero/gameController.java
@@ -15,13 +15,24 @@ public class gameController implements Initializable {
     @FXML
     private Pane cloudPane;
 
+    @FXML
+    private Pane floatPane;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        TranslateTransition translate = new TranslateTransition();
-        translate.setNode(cloudPane);
-        translate.setDuration(Duration.millis(100000));
-        translate.setCycleCount(TranslateTransition.INDEFINITE);
-        translate.setByX((cloudPane.getLayoutX() + cloudPane.getPrefWidth()) * -1);
-        translate.play();
+        TranslateTransition translate1 = new TranslateTransition();
+        translate1.setNode(cloudPane);
+        translate1.setDuration(Duration.millis(100000));
+        translate1.setCycleCount(TranslateTransition.INDEFINITE);
+        translate1.setByX((cloudPane.getLayoutX() + cloudPane.getPrefWidth()) * -1);
+        translate1.play();
+
+        TranslateTransition translate2 = new TranslateTransition();
+        translate2.setNode(floatPane);
+        translate2.setDuration(Duration.millis(5000));
+        translate2.setCycleCount(TranslateTransition.INDEFINITE);
+        translate2.setAutoReverse(true);
+        translate2.setByY(floatPane.getLayoutY() + 10);
+        translate2.play();
     }
 }
