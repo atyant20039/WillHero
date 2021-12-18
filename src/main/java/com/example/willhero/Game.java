0fd8d459a868d3f96b1494654acb21bfee66e112
Ayush Singh <ayush20133@iiitd.ac.in>
2021-12-18 16:58:56 +0530

    Work needed on Loading Screen

--- src/main/java/com/example/willhero/Game.java
+++ src/main/java/com/example/willhero/Game.java
@@ -8,14 +8,27 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
+
+
 public class Game extends Application {
+
+    private Stage myStage;
+
+    private void setStage(Stage stage){
+        this.myStage = stage;
+    }
+
+    public Stage getStage(){
+        return this.myStage;
+    }
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Game.class.getResource("main_menu.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Game.class.getResource("loading-screen.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1000, 600);
         stage.setTitle("WILL HERO");
         stage.getIcons().add(new Image("https://play-lh.googleusercontent.com/Y07T-_5gdE7aUVAgAegAKzeULKTOpXrt4cL9SlKnfnl2qKvBG5oUTNhSb69bEwiLQ4Y"));
         stage.setScene(scene);
+        setStage(stage);
         stage.show();
     }
 
--- src/main/java/com/example/willhero/loadingController.java
+++ src/main/java/com/example/willhero/loadingController.java
@@ -1,5 +1,12 @@
 package com.example.willhero;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
 import javafx.animation.RotateTransition;
 import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
@@ -12,24 +19,44 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
-import javafx.stage.Stage;
 import javafx.util.Duration;
+import org.w3c.dom.Text;
+
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import static java.lang.Thread.sleep;
+
 public class loadingController implements Initializable {
     @FXML
     private ImageView loadingClock;
 
+    @FXML
+    private Button loadingButton;
+
+
+    @FXML
+    private VBox loadingVbox;
+
+    @FXML
+    private void click_to_start(ActionEvent event) throws IOException{
+        loadingButton.getScene().setRoot(FXMLLoader.load(getClass().getResource("main_menu.fxml")));
+    }
+
     @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
+    public void initialize(URL url, ResourceBundle resourceBundle){
         RotateTransition rotate = new RotateTransition();
         rotate.setNode(loadingClock);
         rotate.setDuration(Duration.millis(1000));
         rotate.setCycleCount(5);
         rotate.setByAngle(360);
         rotate.play();
+        loadingVbox.setVisible(false);
+        loadingButton.setDisable(false);
+        loadingButton.setVisible(true);
     }
+
+
 }
